478eb781a570b8d04c28b2c736257105ab00304f
kirill <mrcreepipc@mail.ru>
2025-11-21 21:45:24 +0400

    RA-07

--- app/src/main/java/com/example/androidsprint/CategoriesListFragment.kt
+++ app/src/main/java/com/example/androidsprint/CategoriesListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.example.androidsprint.databinding.FragmentListCategoriesBinding
