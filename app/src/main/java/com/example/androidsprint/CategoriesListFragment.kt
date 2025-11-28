24b86c414b2d586ed9002d7334f7b3909b0797c7
kirill <mrcreepipc@mail.ru>
2025-11-28 12:09:45 +0400

    RA-10

--- app/src/main/java/com/example/androidsprint/CategoriesListFragment.kt
+++ app/src/main/java/com/example/androidsprint/CategoriesListFragment.kt
@@ -10,9 +10,9 @@ import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.example.androidsprint.databinding.FragmentListCategoriesBinding
 
-const val ARG_CATEGORY_ID = "some_"
-const val ARG_CATEGORY_NAME = "some_"
-const val ARG_CATEGORY_IMAGE_URL = "some_"
+const val ARG_CATEGORY_ID = "category_id"
+const val ARG_CATEGORY_NAME = "category_name"
+const val ARG_CATEGORY_IMAGE_URL = "category_image_url"
 
 class CategoriesListFragment : Fragment(R.layout.fragment_list_categories) {
     private var _binding: FragmentListCategoriesBinding? = null
