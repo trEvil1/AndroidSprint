8e79aab86477e4a8d5e5b316158f33ac148f8e43
kirill <mrcreepipc@mail.ru>
2025-12-06 19:40:01 +0400

    RA-12

--- app/src/main/java/com/example/androidsprint/RecipeFragment.kt
+++ app/src/main/java/com/example/androidsprint/RecipeFragment.kt
@@ -1,21 +1,16 @@
 package com.example.androidsprint
 
-import android.R
-import android.app.FragmentManager
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.setFragmentResult
-import androidx.fragment.app.setFragmentResultListener
+
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.androidsprint.databinding.RecipeFragmentBinding
-import com.google.android.material.divider.MaterialDivider
-import com.google.android.material.divider.MaterialDividerItemDecoration
+
 
 class RecipeFragment : Fragment() {
     private var _binding: RecipeFragmentBinding? = null
