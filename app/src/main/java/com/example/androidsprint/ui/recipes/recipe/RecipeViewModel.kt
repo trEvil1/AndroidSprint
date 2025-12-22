4f49adef3f1b3bd1ed980a898a499a53a987f91a
KirillK <lovepgg228@mail.ru>
2025-12-22 08:39:02 +0400

    RA-22

--- app/src/main/java/com/example/androidsprint/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/example/androidsprint/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,11 +1,18 @@
 package com.example.androidsprint.ui.recipes.recipe
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.androidsprint.model.Ingredient
 
-class RecipeViewModel: ViewModel() {
+class RecipeViewModel : ViewModel() {
     data class RecipeState(
-        val recipeName : String? = null,
+        val recipeName: String? = null,
         val ingredients: List<Ingredient> = emptyList()
     )
+
+    private val mutableSelectedItem = MutableLiveData<RecipeState>()
+    var insideSelectedItem: LiveData<RecipeState> = mutableSelectedItem
+    val outsideSelectedItem = insideSelectedItem
+
 }
