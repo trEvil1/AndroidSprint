229f21a693118dd8b76c4aa94cd70e5c4c9dcdfd
KirillK <lovepgg228@mail.ru>
2026-01-18 13:53:40 +0400

    RA-26

--- app/src/main/java/com/example/androidsprint/ui/recipes/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/androidsprint/ui/recipes/recipe/RecipeFragment.kt
@@ -63,7 +63,7 @@ class RecipeFragment : Fragment() {
 
         binding.sbPortions.setOnSeekBarChangeListener(
             PortionSeekBarListener {
-                viewModel.onPortionsCountChanged(viewModel.onPortionsCountChanged(7))
+                viewModel.onPortionsCountChanged(it)
             })
 
         val methodAdapter = MethodAdapter()
