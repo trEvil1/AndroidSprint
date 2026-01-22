53f115cb10d0967de5dddbac7cf21ef928c5c59e
KirillK <lovepgg228@mail.ru>
2026-01-22 20:58:50 +0400

    RA-29

--- app/src/main/java/com/example/androidsprint/ui/recipes/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/androidsprint/ui/recipes/favorites/FavoritesFragment.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.example.androidsprint.R
-import com.example.androidsprint.data.ARG_RECIPE
 import com.example.androidsprint.databinding.FavoritesFragmentBinding
 import com.example.androidsprint.ui.recipes.recipe_list.RecipeListAdapter
+import com.example.androidsprint.ui.recipes.recipe_list.RecipeListFragmentDirections
 
 class FavoritesFragment : Fragment() {
     private var _binding: FavoritesFragmentBinding? = null
@@ -64,7 +62,8 @@ class FavoritesFragment : Fragment() {
     }
 
     private fun openRecipeByRecipeId(recipeId: Int) {
-        val bundle = bundleOf(ARG_RECIPE to recipeId)
-        findNavController().navigate(R.id.recipeFragment, bundle)
+        val action =
+            RecipeListFragmentDirections.actionRecipeListFragmentToRecipeFragment2(recipeId.toString())
+        findNavController().navigate(action)
     }
 }
