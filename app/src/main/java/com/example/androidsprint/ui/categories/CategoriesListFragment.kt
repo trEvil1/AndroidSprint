0d90aefa574f106b8d070fa6d80b94b91d3a67b9
KirillK <lovepgg228@mail.ru>
2026-01-23 15:39:27 +0400

    RA-29

--- app/src/main/java/com/example/androidsprint/ui/categories/CategoriesListFragment.kt
+++ app/src/main/java/com/example/androidsprint/ui/categories/CategoriesListFragment.kt
@@ -4,18 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
-import com.example.androidsprint.R
-import com.example.androidsprint.data.ARG_CATEGORY_ID
-import com.example.androidsprint.data.ARG_CATEGORY_IMAGE_URL
-import com.example.androidsprint.data.ARG_CATEGORY_NAME
 import com.example.androidsprint.databinding.FragmentListCategoriesBinding
-import com.example.androidsprint.model.Category
-import com.example.androidsprint.ui.recipes.recipe_list.RecipeListFragmentDirections
 
 class CategoriesListFragment : Fragment() {
     private var _binding: FragmentListCategoriesBinding? = null
@@ -24,7 +16,6 @@ class CategoriesListFragment : Fragment() {
             "Binding for FragmentListCategoriesBinding must not be null"
         )
     val viewModel: CategoriesListViewModel by viewModels()
-    val args: CategoriesListFragmentArgs by navArgs<CategoriesListFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -68,16 +59,11 @@ class CategoriesListFragment : Fragment() {
     fun openRecipesByCategoryId(categoryId: Int) {
         val category =
             viewModel.categoryLiveData.value?.categoriesList?.find { it.id == categoryId }
-        val categoryName = category?.title
-        val categoryImageUrl = category?.imageUrl
-        val bundle = bundleOf(
-            ARG_CATEGORY_ID to categoryId,
-            ARG_CATEGORY_NAME to categoryName,
-            ARG_CATEGORY_IMAGE_URL to categoryImageUrl
-        )
         val action =
             CategoriesListFragmentDirections.actionCategoriesListFragmentToRecipeListFragment(
-                categoryId.toString()
+                category ?: throw IllegalArgumentException(
+                    "Category with id $categoryId not found"
+                )
             )
         findNavController().navigate(action)
     }
