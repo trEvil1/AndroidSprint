ec265d1ba3a910420b17184072042981a687a790
KirillK <lovepgg228@mail.ru>
2026-01-26 12:57:04 +0400

    RA-32

--- app/src/main/java/com/example/androidsprint/MainActivity.kt
+++ app/src/main/java/com/example/androidsprint/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.androidsprint
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import com.example.androidsprint.databinding.ActivityMainBinding
+import com.google.gson.Gson
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -15,6 +21,22 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        val thread = Thread {
+            val url = URL("https://recipes.androidsprint.ru/api/category")
+            val connection = url.openConnection() as HttpURLConnection
+            connection.connect()
+            val json = connection.getInputStream().bufferedReader().readText()
+
+            val categoryList = Gson().fromJson(json, CategoryItem::class.java)
+
+            Log.i("!!!", "Выполняю запрос на потоке :${connection.url}")
+            Log.i("!!!", "Метод onCreate() выполняется на потоке: :${connection.url}")
+            Log.i("!!!", " :${json}")
+        }
+        thread.start()
+
         val view = binding.root
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -37,3 +59,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+@Serializable
+data class CategoryItem(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val imageUrl: String
+)
