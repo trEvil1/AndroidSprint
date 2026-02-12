c439b52976d80bcb91d94bfb5071560ab66cf464
KirillK <lovepgg228@mail.ru>
2026-02-12 20:18:12 +0400

    RA-40

--- app/src/main/java/com/example/androidsprint/model/CategoryDao.kt
+++ app/src/main/java/com/example/androidsprint/model/CategoryDao.kt
@@ -1,21 +1,16 @@
 package com.example.androidsprint.model
 
 import androidx.room.Dao
-import androidx.room.Database
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.RoomDatabase
-import androidx.room.TypeConverter
 
 @Dao
 interface CategoryDao {
-    @TypeConverter
     @Query("SELECT * FROM category")
     suspend fun getAll(): List<Category>
 
-    @TypeConverter
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(categories: List<Category>)
 
