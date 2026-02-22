4d01f882980d47b10c2446e18ba947fd34183309
KirillK <lovepgg228@mail.ru>
2026-02-22 11:48:20 +0400

    RA-41

--- app/src/main/java/com/example/androidsprint/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/example/androidsprint/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,20 +1,17 @@
 package com.example.androidsprint.ui.recipes.recipe
 
 import android.app.Application
-import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.androidsprint.RecipeRepository
-import com.example.androidsprint.data.KEY_FAVORITE_PREFS
-import com.example.androidsprint.data.KEY_PREFERENCE_FILE
 import com.example.androidsprint.model.Recipe
 import kotlinx.coroutines.launch
 
 class RecipeViewModel(application: Application) : AndroidViewModel(application) {
     data class RecipeState(
-        val isFavorite: Boolean = false,
+        val isFavorite: Boolean,
         val portionCount: Int = 1,
         val recipe: Recipe? = null,
     )
@@ -26,39 +23,27 @@ class RecipeViewModel(application: Application) : AndroidViewModel(application) 
     fun loadRecipe(recipeId: Int) {
         viewModelScope.launch {
             val recipe = recipeRepository.getRecipeById(recipeId)
-            val favorites = getFavorites()
             val currentPortions = _recipeLiveData.value?.portionCount ?: 1
             _recipeLiveData.postValue(
                 RecipeState(
                     recipe = recipe,
-                    isFavorite = recipe?.id.toString() in favorites,
+                    isFavorite = recipe?.isFavorite ?: false,
                     portionCount = currentPortions,
                 )
             )
         }
     }
 
-    private fun getFavorites(): MutableSet<String> {
-        val sp = getApplication<Application>().applicationContext.getSharedPreferences(
-            KEY_PREFERENCE_FILE, Context.MODE_PRIVATE
-        )
-        return HashSet(
-            sp?.getStringSet(
-                KEY_FAVORITE_PREFS, HashSet<String>()
-            ) ?: mutableSetOf()
-        )
-    }
-
     fun onFavoriteClicked() {
-        val currentState = _recipeLiveData.value ?: return
-        val recipe = currentState.recipe ?: return
-        val newFavoriteState = !currentState.isFavorite
         viewModelScope.launch {
+            val currentState = _recipeLiveData.value
+            val recipe = currentState?.recipe ?:return@launch
+            val newFavoriteState = !currentState.isFavorite
             recipeRepository.updateRecipe(
                 recipe.copy(isFavorite = newFavoriteState)
             )
+            _recipeLiveData.value = currentState.copy(isFavorite = newFavoriteState)
         }
-        _recipeLiveData.value = currentState.copy(isFavorite = newFavoriteState)
     }
 
 
